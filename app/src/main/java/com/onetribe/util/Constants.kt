c69d186589788d63f9521ad1b6bfc82045b5e826
QuintinAdam <quintinjadam@gmail.com>
2021-02-04 14:29:05 -0600

    updates links

--- app/src/main/java/com/onetribe/util/Constants.kt
+++ app/src/main/java/com/onetribe/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.onetribe.util
 
-const val BASE_URL = "https://turbo-native-demo.glitch.me"
+const val BASE_URL = "https://www.onetribe.co"
 const val HOME_URL = BASE_URL
-const val SIGN_IN_URL = "$BASE_URL/signin"
-const val NUMBERS_URL = "$BASE_URL/numbers"
+const val SIGN_IN_URL = "$BASE_URL/sign-in"
+const val NUMBERS_URL = "$BASE_URL/turbo/numbers"
